7e17be1eae981b585453f457912bfdefd285e2f1
Шантажная Пена <45635607+AlexeyTolstopyatov@users.noreply.github.com>
2024-08-03 18:28:34 +0700

    Update registers.h
    КОДИРОВКА

--- src/registers.h
+++ src/registers.h
@@ -6,50 +6,47 @@
 
 
 
-/*Переопределения типов*/
+/*РџРµСЂРµРѕРїСЂРµРґРµР»РµРЅРёРµ С‚РёРїРѕРІ РґР°РЅРЅС‹С…*/
 typedef unsigned short ushort_t;
 typedef unsigned char  bool_t;
 typedef unsigned char  byte_t;
 
+/*РњР°РєСЂРѕ РѕРїСЂРµРґРµР»РµРЅРёСЏ РїСЂРёС‡РёРЅ РІС‹С…РѕРґР°*/
 #define BUFFER_PROBLEM  -4
 #define ALLOC_PROBLEM	-3
 #define FILE_PROBLEM	-2
 #define SYNTAX_PROBLEM	-1
 
 /*
- * Королевские размеры
+ * РњР°РєСЃРёРјР°Р»СЊРЅС‹Р№ СЂР°Р·РјРµСЂ
  */
 #define MAX 65535
 
-/*	Регистры Общего назначения
- * Состоят из старших (High) 8 бит и младших (Low) 8 бит.
- * [AX] = [AH|AL]. Размер регистра = 2 байта (16 бит)
- * Поэтому 16-разрядные регистры.
+/* Р РµРіРёСЃС‚СЂС‹ РѕР±С‰РµРіРѕ РЅР°Р·РЅР°С‡РµРЅРёСЏ
+ * СЃРѕСЃС‚РѕСЏС‚ РёР· СЃС‚Р°СЂС€РёС… (High) 8 Р±РёС‚ Рё РњР»Р°РґС€РёС… (Low) 8 Р±РёС‚. 
+ * [AX] = [AH|AL]
  */
 ushort_t ax; /*Accumulative register*/
 ushort_t bx; /*Based register*/
 ushort_t cx; /*Count register*/
 ushort_t dx; /*Data register */
 
-/*	Сегментные регистры
- * предназначены для того, чтобы указать на сегменты, 
- * к которым программа имеет доступ в конкретный момент времени.
+/* Р РµРіРёСЃС‚СЂС‹ СЃРµРіРјРµРЅС‚РѕРІ
+ * 
  */
 ushort_t cs; /*CODE Segment */
 ushort_t ds; /*DATA Segment */
 ushort_t es; /*EXTRA Segment*/
 ushort_t ss; /*STACK Segment*/
 
-/*	Указатели
- * регисты, применяемые для хранения 
- * адреса памяти в архитектуре процессора
+/*	РЈРєР°Р·Р°С‚РµР»Рё
  */
 ushort_t ip; /*Instruction Pointer*/
 ushort_t sp; /*Stack Pointer*/
 ushort_t bp; /*Base Pointer */
 
 /*
- * Флаги
+ * Р¤Р»Р°РіРё РїСЂРѕС†РµСЃСЃРѕСЂР°
  */
 ushort_t zf; /**/
 ushort_t pf; /*Pointer flag*/
